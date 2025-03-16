ecd24af411bf4f48f64af46a094537678feb3024
exuteboll <exuteboll4@gmail.com>
2025-03-16 11:59:58 +0200

    Add files via upload

---
+++ ConsoleApp17/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int Year { get { return year; } set { year = value; } }
+
+        public Car() { }
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Марка: {make}\t модель: {model}\t год: {year}");
+        }
+    }
+}
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] car = new Car[]
+            {
+                new Car("asd", "asd", 1245),
+                new Car("afsf","dsg",1325),
+                new Car("hjk","weot", 0992)
+            };
+            foreach(Car p in car)
+            { 
+                p.Print();
+            }
+            Car[] car1 = new Car[1];
+            for(int i = 0; i < car1.Length; i++) 
+                {
+                Console.WriteLine($"Введите марку/модель/год: ");
+                string marka = Console.ReadLine();
+                string model = Console.ReadLine();
+                int year = int.Parse(Console.ReadLine());
+                car1[i] = new Car(marka, model, year);
+            }
+            foreach (Car car1p in car1)
+            {
+                car1p.Print();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
